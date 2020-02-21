9f6e14997176a0814a803b69657998d9a05a7df5
chris huntley <cjhuntley4@gmail.com>
2020-02-21 14:28:22 -0600

    WE MADE IT

---
+++ ic.js
@@ -0,0 +1,43 @@
+// Ex 1***************************************
+// for(let i=-25; i<=20; i++){
+//     console.log(i)
+// }
+// Ex 2*****************************************
+// let question=prompt('what is your first and last name')
+// function firstandlast(first,last){
+
+//     console.log(`Welcome ${last},${first}`)
+// }
+// firstandlast(question)
+// Ex 3**********************************************
+// let number=parseInt(prompt('Enter a number'))
+// function Max(maximum){
+//     for(let i=0;i<=maximum; i++)
+//     console.log(i)
+// }
+
+// Max(number);
+// Ex 4*************************************************
+// let password=prompt('Password Here')
+// let password2=prompt('Password Here')
+// function checkpassword() {
+//     let pw1 = password
+//     let pw2 = password2
+    
+//     if (pw1 == pw2) { console.log('True') }
+//     else{
+//         console.log('False')
+//     }
+
+
+// }
+// checkpassword(password)
+// Ex 5********************************************************
+// function sumof3numbers(arraynumbers){
+//     console.log(`${arraynumbers}`);
+// }
+// let numbers = [5,8,12];
+// for(let i = 0; i < numbers.length; i++){
+//     sumof3numbers(numbers[i], i)
+//  }
+
